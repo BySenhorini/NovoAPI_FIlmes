157e3e6cfc132fa06a1e1d605f824b7f7c74f612
YASMIN SENHORINI DA SILVA <56805860881@info.local>
2025-02-27 16:56:54 -0300

    Subindo Projeto

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_filmes_senai1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
